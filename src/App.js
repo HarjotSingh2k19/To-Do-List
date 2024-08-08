59a00b8747043e7780f23cbfeca69483dce8cdeb
Harjot Singh <harjotsingh2k19@gmail.com>
2024-08-08 18:02:49 +0530

    Adding title and description functionality completed

--- src/App.js
+++ src/App.js
@@ -1,53 +1,106 @@
-import './App.css';
-import React, {useState} from "react";
+import "./App.css";
+import React, { useEffect, useState } from "react";
 import { MdDelete } from "react-icons/md";
 import { FaCheck } from "react-icons/fa";
 
 function App() {
-
   const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+  const [allTodos, setTodos] = useState([]);
+  const [newTitle, setNewTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+
+  const handleAddTodo = () => {
+    let newTodoItem = {
+      title: newTitle,
+      description: newDescription,
+    };
+
+    let updatedTodoArr = [...allTodos];
+    updatedTodoArr.push(newTodoItem);
+
+    setTodos(updatedTodoArr);
+
+    localStorage.setItem('todolist', JSON.stringify(updatedTodoArr));
+  };
+
+  useEffect(() => {
+    let savedTodo = JSON.parse(localStorage.getItem('todolist'));
+
+    if(savedTodo){
+      setTodos(savedTodo);
+    }
+    
+  },[]);
 
   return (
     <div className="App">
       <h1>My Todos</h1>
 
-      <div className = 'todo-wrapper'>
-        <div className = 'todo-input'>
-          <div className = 'todo-input-item'>
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
             <label>Title</label>
-            <input type="text" placeholder = "What's the task title?"/>
+            <input
+              type="text"
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
+              placeholder="What's the task title?"
+            />
           </div>
-          <div className = 'todo-input-item'>
+          <div className="todo-input-item">
             <label>Description</label>
-            <input type="text" placeholder = "What's the task description?"/>
+            <input
+              type="text"
+              value={newDescription}
+              onChange={(e) => setNewDescription(e.target.value)}
+              placeholder="What's the task description?"
+            />
           </div>
-          <div className = 'todo-input-item'>
-              <button type = 'button' className ='primaryBtn'>Add</button>
+          <div className="todo-input-item">
+            <button
+              type="button"
+              onClick={handleAddTodo}
+              className="primaryBtn"
+            >
+              Add
+            </button>
           </div>
         </div>
-      
 
-      {/* Button Area */}
-      <div className = 'btn-area'>
-        <button className = {`secondaryBtn ${isCompleteScreen === false && 'active'}`} onClick={() => setIsCompleteScreen(false)}>Todo</button>
-        <button className = {`secondaryBtn ${isCompleteScreen === true && 'active'}`} onClick={() => setIsCompleteScreen(true)}>Completed</button>
-      </div>
-
-      {/* Actual Content Area*/}
-      <div className = 'todo-list'>
-        <div className = 'todo-list-item'>
-          <h3>Task1</h3>
-          <p>Description</p>
-        
-
-        <div>
-        <MdDelete className='icon'/>
-        <FaCheck className='check-icon'/>
+        {/* Button Area */}
+        <div className="btn-area">
+          <button
+            className={`secondaryBtn ${isCompleteScreen === false && "active"}`}
+            onClick={() => setIsCompleteScreen(false)}
+          >
+            Todo
+          </button>
+          <button
+            className={`secondaryBtn ${isCompleteScreen === true && "active"}`}
+            onClick={() => setIsCompleteScreen(true)}
+          >
+            Completed
+          </button>
         </div>
 
-        </div>
-      </div>
+        {/* Actual Content Area*/}
+        <div className="todo-list">
+          {allTodos.map((item, index) => {
+            return (
+              <div className="todo-list-item" key={index}>
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                </div>
 
+                <div>
+                  <MdDelete className="icon" />
+                  <FaCheck className="check-icon" />
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
